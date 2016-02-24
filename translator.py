141bfe568f264b34b5b8d2725d7fbe1065393c9c
mhamilton723 <mark.hamilton@yale.edu>
2016-02-23 21:34:25 -0500

    fixes
    ^

--- translator.py
+++ translator.py
@@ -1,21 +1,16 @@
-# coding=utf-8
+import pickle
+import math
+from embedding import *
+import numpy as np
+from sklearn.base import clone
+
 __author__ = 'Mark'
 
-import matplotlib
+class Translator(object):
 
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import math
+    def __init__(self):
+        pass
 
-from embedding import *
-
-from sklearn.linear_model import LinearRegression
-from sklearn.cross_validation import train_test_split
-from sklearn.manifold import TSNE, Isomap, LocallyLinearEmbedding, MDS, SpectralEmbedding
-from sklearn.decomposition import TruncatedSVD, PCA, KernelPCA, SparsePCA, DictionaryLearning
-from sklearn.base import clone
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
 
 en_2_es = pickle.load(open('data/en_2_es.pkl', 'r'))
 es_2_en = pickle.load(open('data/es_2_en.pkl', 'r'))
@@ -186,108 +181,3 @@ def regression_translation(dictionary=en_2_es, embedding1=en_embedding, embeddin
         train_dict[word1] = trans
 
     return train_dict, test_dict
-
-
-def get_words_of_cat(cat, dict=en_2_es, language='both'):
-    english_words = set(en for en, (es, c) in dict.iteritems() if cat in c)
-    spanish_words = list(es for en, (es, c) in dict.iteritems() if cat in c)
-    spanish_words = reduce(lambda x, y: x | y, spanish_words)
-    if language == 'both':
-        return english_words, spanish_words
-    if language == 'en':
-        return english_words
-    elif language == 'es':
-        return spanish_words
-    else:
-        raise ValueError("Need to enter in valid language param")
-
-
-# def get_complement_of_cats(cats,dict,language='both'):
-
-
-def plot_comp(model=TruncatedSVD(), subsample=1000, alpha=.005, alpha_cat=.5,
-              cats=['color', 'profession', 'science', 'transport'], dict=en_2_es,
-              en_embeddings=en_embedding, es_embeddings=es_embedding):
-    colors = ['r', 'g', 'k', 'y']
-    names = ["english", "spanish"]
-    embeddings = [en_embedding, es_embedding]
-
-    if subsample is not None:
-        en_cat_words = reduce(lambda x, y: x | y, [get_words_of_cat(cat, dict, 'en') for cat in cats])
-        es_cat_words = reduce(lambda x, y: x | y, [get_words_of_cat(cat, dict, 'es') for cat in cats])
-        cat_words = [en_cat_words, es_cat_words]
-        embeddings = [subsample_embedding(emb, subsample, cat_word)
-                      for emb, cat_word in zip(embeddings, cat_words)]
-
-    plt.figure(figsize=(15, 8))
-    for i, emb in enumerate(embeddings):
-        if model is not None:
-            model_copy = clone(model)
-            emb = transform(emb, model_copy)
-
-        plt.subplot(1, len(embeddings), i + 1)
-        plt.scatter(emb.embeddings[:, 0], emb.embeddings[:, 1], alpha=alpha)
-        for j, cat in enumerate(cats):
-            language_cat_words = get_words_of_cat(cat, dict)
-            cat_emb = [emb.word_to_embedding(word) for word in language_cat_words[i]]
-            # note: line above based on the output of get words of cat
-            cat_emb = np.array(filter(lambda x: x is not None, cat_emb))
-
-            plt.scatter(cat_emb[:, 0], cat_emb[:, 1], alpha=alpha_cat, color=colors[j], label=cat)
-        plt.title("Projection onto {} of {} words".format(str(model_copy).split('(')[0], names[i]))
-    plt.legend()
-    plt.savefig(
-        'plots/' +
-        str(model_copy).split('(')[0] +
-        '_' + str(subsample // 1000) +
-        'k_embedding.png', dpi=100)
-
-
-run_knn = False
-if run_knn:
-    print en_embedding.knn("Dog")
-    print en_embedding.knn("blue")
-    print es_embedding.knn("perro")
-    print en_embedding.knn("azul")
-
-try_comp = False
-if try_comp:
-    # model = Pipeline([('Scaler', StandardScaler()), ('embedder', TruncatedSVD(n_components=2))])
-    model = TruncatedSVD(n_components=2)
-    # model = TSNE(n_components=2)
-
-    # model = MDS(n_components=2)
-    plot_comp(model=model, subsample=1000, alpha=.01)
-    # embedding_translation = nn_embedding_translate()
-
-try_regression_translate = True
-if try_regression_translate:
-    model = LinearRegression()
-
-    regen_dicts = True
-    filename = 'data/translations.pkl'
-    if regen_dicts:
-        train_trans, test_trans = regression_translation(model=model)
-        pickle.dump((train_trans, test_trans), open(filename, 'w+'))
-    else:
-        train_trans, test_trans = pickle.load(open(filename, 'r'))
-
-    print "training translation with model:{} scored:{}".format(model, translation_quality(train_trans))
-    print "testing translation with model:{} scored:{}".format(model, translation_quality(test_trans))
-
-try_search = False
-if try_search:
-    dims = [2, 5, 10, 20, 30, None]
-    for dim in dims:
-        pre_transforms = [TruncatedSVD(n_components=dim),
-                          PCA(n_components=dim),
-                          # KernelPCA(n_components=dim, kernel='rbf'), #TODO fix memory error / try on larger machine
-                          SparsePCA(n_components=dim)]  # ,
-        # DictionaryLearning(n_components=dim)]
-
-        for pre_transform in pre_transforms:
-            embedding_translation = nn_embedding_translate(pre_transform=pre_transform)
-            print "Translation with pt:{} scored:{}".format(pre_transform,
-                                                            translation_quality(embedding_translation))
-
-    print translation_quality(en_2_es)
